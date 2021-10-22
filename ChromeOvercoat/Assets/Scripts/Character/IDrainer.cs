c8cb5123a6f9f9c4be4484c90fc24c162259a2d5
Kilian12345 <allairekilian@gmail.com>
2021-10-22 15:48:45 +0200

    LA PRISON

--- ChromeOvercoat/Assets/Scripts/Character/IDrainer.cs
+++
@@ -1,8 +0,0 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public interface IDrainer
-{
-    abstract void SetDrainableTarget(IDrainable target, bool onRange);
-}
